b3690afb4cbd1dacbf5478ba399bfc01ae7d8c8a
Denis Shvyrkov <denysbc2011@gmail.com>
2024-03-30 22:10:55 -0700

    created components

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,22 @@
+import OptionsItem from '../OptionsItem/OptionsItem';
+
+const Options = () => {
+  return (
+    <ul>
+      <li>
+        <OptionsItem />
+      </li>
+      <li>
+        <OptionsItem />
+      </li>
+      <li>
+        <OptionsItem />
+      </li>
+      <li>
+        <OptionsItem />
+      </li>
+    </ul>
+  );
+};
+
+export default Options;
---
+++ src/components/OptionsItem/OptionsItem.jsx
@@ -0,0 +1,5 @@
+const OptionsItem = () => {
+  return <button></button>;
+};
+
+export default OptionsItem;
